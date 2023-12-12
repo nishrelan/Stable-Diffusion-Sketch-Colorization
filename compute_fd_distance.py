average clip scores, fix argparse bugs

compute_fd_distance.py
@@ -40,6 +40,8 @@ def sqrtm(tensor, disp=True):
     sqrtm = torch.from_numpy(res.real).to(tensor)
     return sqrtm
 
+# This function was modified, but is based on
+# https://github.com/mseitzer/pytorch-fid/blob/master/src/pytorch_fid/fid_score.py#L243
 def compute_fd(gen_stats, target_stats, eps=1e-6):
     with torch.no_grad():
         mu1, sigma1 = gen_stats
@@ -76,18 +78,19 @@ def main():
     model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14").to(device)
     image_processor = AutoProcessor.from_pretrained("openai/clip-vit-large-patch14")
 
+    with torch.no_grad():
     # get the images from corresponding directories
-    generation_images = get_images_from_directory(args.generations)
-    target_images = get_images_from_directory(args.targets)
+        generation_images = get_images_from_directory(args.generations)
+        target_images = get_images_from_directory(args.targets)
 
-    generation_input = image_processor(images=generation_images, return_tensors='pt')
-    target_input = image_processor(images=target_images, return_tensors='pt')
+        generation_input = image_processor(images=generation_images, return_tensors='pt')
+        target_input = image_processor(images=target_images, return_tensors='pt')
 
-    generation_input = generation_input.to(device)
-    target_input = target_input.to(device)
+        generation_input = generation_input.to(device)
+        target_input = target_input.to(device)
 
-    gen_features = model.get_image_features(**generation_input)
-    target_features = model.get_image_features(**target_input)
+        gen_features = model.get_image_features(**generation_input)
+        target_features = model.get_image_features(**target_input)
 
     print("Computing FD score")
     gen_stats = get_mean_and_cov(gen_features)

get_clip_score.py
@@ -10,8 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-d', type=str, required=True)
 
 def normalize(tensor):
-    with torch.no_grad():
-        return tensor / tensor.norm(p=2, dim=1, keepdim=True)
+    return tensor / tensor.norm(p=2, dim=1, keepdim=True)
 
 def main():
     args = parser.parse_args()
@@ -28,7 +27,7 @@ def main():
 
     images = []
     for filename in os.listdir(directory):
-        if filename.endswith('.png'):
+        if filename.endswith('.png') or filename.endswith('.jpg'):
             print(filename)
             filepath = os.path.join(directory, filename)
             with open(filepath, 'rb') as f:
@@ -38,26 +37,32 @@ def main():
     # get images as tensors and then run inference on the clip image
     # model to get image embeddings
     im_input = image_processor(images=images, return_tensors='pt')
-    im_input = im_input.to(device)
-    im_features = model.get_image_features(**im_input)
+    with torch.no_grad():
+        im_input = im_input.to(device)
+        im_features = model.get_image_features(**im_input)
 
 
-    prompt2 = "a black and white image"
-    prompt3 = "an image with color"
-    prompt6 = "a photo of a pokemon"
-    prompt7 = "a colored photo of a pokemon"
+        prompt2 = "a black and white image"
+        prompt3 = "an image with color"
+        prompt6 = "a photo of a pokemon"
+        prompt7 = "a colored photo of a pokemon"
 
-    text_inputs = tokenizer([prompt2, prompt3, prompt6, prompt7], padding=True, return_tensors='pt')
-    text_inputs = text_inputs.to(device)
-    text_features = model.get_text_features(**text_inputs)
+        text_inputs = tokenizer([prompt2, prompt3, prompt6, prompt7], padding=True, return_tensors='pt')
+        text_inputs = text_inputs.to(device)
+        text_features = model.get_text_features(**text_inputs)
 
-    # compute cosine similarities between the prompt embedding and each image
-    im_features = normalize(im_features)
-    text_features = normalize(text_features)
+        # compute cosine similarities between the prompt embedding and each image
+        im_features = normalize(im_features)
+        text_features = normalize(text_features)
 
-    similarities = im_features @ text_features.T
+        similarities = im_features @ text_features.T
 
-    print(similarities)
+        mean_similarity = similarities.mean(dim=0)
+
+        print(mean_similarity)
+
+    
+
 
     
 